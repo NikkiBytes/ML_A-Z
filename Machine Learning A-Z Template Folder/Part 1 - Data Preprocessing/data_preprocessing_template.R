8f035d99173e4247d700e586463cd7ee1b0ae358
NikkiBytes <nicholletteacosta@gmail.com>
2018-11-26 23:08:25 -0500

    added encoding example in R

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -15,3 +15,12 @@ dataset$Age = ifelse(is.na(dataset$Age),
 dataset$Salary = ifelse(is.na(dataset$Salary),
                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Salary)
+
+# Encoding categorical data
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'), #create vector of 3 elements
+                         labels = c(1, 2, 3)) #create labels to encode to 
+dataset$Purchased = factor(dataset$Purchased,
+                         levels = c('No', 'Yes'), #create vector of 3 elements
+                         labels = c(0, 1)) #create labels to encode to 
+                         
