be93a7eb7c581ccf3a2cc04f3eb8431f2bf6f393
NikkiBytes <nicholletteacosta@gmail.com>
2018-11-26 23:33:39 -0500

    how to split train and test set in python and R

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -39,3 +39,4 @@ y = labelencoder_y.fit_transform(y)
 # Spliting dataset into train and test set 
 from sklearn.model_selection import train_test_split 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
