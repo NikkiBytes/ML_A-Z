d07ac284f23d5aa5b468b9cbcecd68c4603fc545
NikkiBytes <nicholletteacosta@gmail.com>
2018-11-28 16:51:23 -0500

    finished python example of linear regression

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.py
@@ -21,3 +21,29 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting Simple Linear Regression to the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predicting the test set results 
+y_pred = regressor.predict(X_test)
+#y_test is real salaries, y_pred is the prediction
+
+# Visualizing the training set results
+plt.scatter(X_train, y_train, color="red")
+plt.plot(X_train, regressor.predict(X_train), color="pink")
+plt.title("Salary vs. Experience (Training Set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
+
+# Regressor is trained on training, don't need to make it again
+# Visualizing the Test set results
+plt.scatter(X_test, y_test, color="red")
+plt.plot(X_train, regressor.predict(X_train), color="pink")
+plt.title("Salary vs. Experience (Test Set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
