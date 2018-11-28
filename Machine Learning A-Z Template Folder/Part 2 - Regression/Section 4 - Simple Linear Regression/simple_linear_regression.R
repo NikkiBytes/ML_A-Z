8dc37351724d1daae5305d57a204dd1ed63020a4
NikkiBytes <nicholletteacosta@gmail.com>
2018-11-28 17:29:46 -0500

    added completed linear regression example in R

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.R
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.R
@@ -1,4 +1,4 @@
-# Simple Linear Regression
+# Data Preprocessing Template
 
 # Importing the dataset
 dataset = read.csv('Salary_Data.csv')
@@ -11,35 +11,34 @@ split = sample.split(dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
-
-# Fitting Simple Linear Regression to the Training set
+# Fitting Simple Linear Regression to the Training Set 
 regressor = lm(formula = Salary ~ YearsExperience,
-               data = training_set)
+               data = training_set) #Means salary is proportianl to years experience
+
+summary(regressor) 
 
 # Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
 
-# Visualising the Training set results
+# Visualizing the Training Set results
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
-             colour = 'red') +
-  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
-            colour = 'blue') +
-  ggtitle('Salary vs Experience (Training set)') +
-  xlab('Years of experience') +
-  ylab('Salary')
+             colour = "red") +
+  geom_line(aes(x = training_set$YearsExperience, y=predict(regressor, newdata = training_set)),
+             colour = "pink") +
+  ggtitle("Salary vs. Experience(Training Set)") +
+  xlab("Years of Experience") +
+  ylab("Salary")
 
-# Visualising the Test set results
-library(ggplot2)
+#Visualizing test set results
 ggplot() +
   geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
-             colour = 'red') +
-  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
-            colour = 'blue') +
-  ggtitle('Salary vs Experience (Test set)') +
-  xlab('Years of experience') +
-  ylab('Salary')
+             colour = "red") +
+  geom_line(aes(x = training_set$YearsExperience, y=predict(regressor, newdata = training_set)),
+            colour = "pink") +
+  ggtitle("Salary vs. Experience(Test Set)") +
+  xlab("Years of Experience") +
+  ylab("Salary")
+
+
--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -1,4 +1,4 @@
-# Simple Linear Regression
+# Data Preprocessing Template
 
 # Importing the libraries
 import numpy as np
@@ -11,7 +11,7 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 # Feature Scaling
@@ -22,26 +22,28 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting Simple Linear Regression to the Training set
+# Fitting Simple Linear Regression to the training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
-# Predicting the Test set results
+#Predicting the test set results 
 y_pred = regressor.predict(X_test)
+#y_test is real salaries, y_pred is the prediction
 
-# Visualising the Training set results
-plt.scatter(X_train, y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Training set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
+# Visualizing the training set results
+plt.scatter(X_train, y_train, color="red")
+plt.plot(X_train, regressor.predict(X_train), color="pink")
+plt.title("Salary vs. Experience (Training Set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
 plt.show()
 
-# Visualising the Test set results
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Test set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
+# Regressor is trained on training, don't need to make it again
+# Visualizing the Test set results
+plt.scatter(X_test, y_test, color="red")
+plt.plot(X_train, regressor.predict(X_train), color="pink")
+plt.title("Salary vs. Experience (Test Set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
 plt.show()
